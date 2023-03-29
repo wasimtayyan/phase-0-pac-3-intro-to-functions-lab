4aab288c6d12b9042c2b78e95626d52586ca962e
wasim altayyan <wasimtayyan@gmail.com>
2023-03-30 02:17:05 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string){
+return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+console.log(string.toUpperCase());
+}
+function logWhisper(streng){
+    console.log(streng.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string ===  "Let's have dinner together!") {
+    return "I would love to!";
+  }
+}
